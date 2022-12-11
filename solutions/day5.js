c0b3889b7f7fdbfdacae7d4f0d1c126547d0adcd
jane <tth17598@gmail.com>
2022-12-11 16:10:15 +0700

    Update: day5

---
+++ solutions/day5.js
@@ -0,0 +1,21 @@
+import { input5 as input } from '../input.js';
+
+const container = input.stack.split('\n  ')
+  .map(i => i.replaceAll('    ', '-').replaceAll(' ', '').replace(/[\[\]]/g, ''));
+
+const stacks = Array.from({ length: container.pop().length }, () => []);
+container.map(item => item.split('').map((c, index) => {
+  if (c !== '-') stacks[index].push(c);
+}));
+
+input.commands.split('\n')
+  .map(command => command.match(/\d+/g, '').map(Number))
+  .map(([length, source, target]) => {
+    // remove reverse() for part 2
+    stacks[target - 1] = [...stacks[source - 1].splice(0, length).reverse(), ...stacks[target - 1]]
+  });
+
+const ans = stacks.map(i => i[0]).join('');
+
+console.log('Answer', ans);
+
