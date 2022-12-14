6d9cc389d0260a8b691bf0a7ee06a3388182a6c1
jane <tth17598@gmail.com>
2022-12-14 15:39:36 +0700

    Update: day 11

---
+++ solutions/day11.js
@@ -0,0 +1,59 @@
+import { input11 as input } from '../input.js';
+
+const extractNumber = (string) => {
+  return +string.replace(/\D/g,'');
+}
+
+const extractItems = (string) => {
+  return string.split(': ')[1].split(', ').map(Number);
+}
+
+const extractOperation = (string) => {
+  return string.split(': ')[1].split('= ')[1];
+}
+
+const operate = (operation, val, test, divider) => {
+  const value = eval(operation.replaceAll('old', val));
+  const level = divider ? value % divider : value / 3;
+  return [level, level % test];
+}
+
+const monkeys = input.split('\n\n').map(text => {
+  const [id, items, operation, test, trueAction, falseAction] = text.split('\n');
+
+  return {
+    id: extractNumber(id),
+    items: extractItems(items),
+    operation: extractOperation(operation),
+    test: extractNumber(test),
+    trueAction: extractNumber(trueAction),
+    falseAction: extractNumber(falseAction),
+    inspectCounter: 0,
+  };
+});
+
+const run = (numOfRound, divider) => {
+  for (let i = 0; i < numOfRound; i++) {
+    monkeys.map(monkey => {
+      const { operation, test, items, trueAction, falseAction } = monkey;
+      while (items.length) {
+        const item = items.shift();
+        const [level, result] = operate(operation, item, test, divider);
+
+        if (result === 0) monkeys[trueAction].items.push(level);
+        else monkeys[falseAction].items.push(level);
+        monkey.inspectCounter++;
+      }
+    });
+  }
+}
+
+const divider = monkeys.reduce((res, cur) => res * cur.test, 1);
+
+// part 2
+run(10000, divider);
+
+const inspectCounter = monkeys.map(monkey => monkey.inspectCounter).sort((a, b) => b - a);
+const ans = inspectCounter[0] * inspectCounter[1];
+
+console.log('Answer', ans, monkeys);
