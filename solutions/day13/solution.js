6e32463ecb930961a250e9ec48b2a657474447e9
jane <tth17598@gmail.com>
2023-02-14 14:56:32 +0700

    Update: day13 part 1

--- solutions/day13/solution.js
+++ solutions/day13/solution.js
@@ -2,84 +2,57 @@ import fs from 'fs';
 
 const input = fs.readFileSync('./input.txt').toString().trim();
 
-const test = `[1,1,3,1,1]
-[1,1,5,1,1]
-
-[[1],[2,3,4]]
-[[1],4]
-
-[9]
-[[8,7,6]]
-
-[[4,4],4,4]
-[[4,4],4,4,4]
-
-[7,7,7,7]
-[7,7,7]
-
-[]
-[3]
-
-[[[]]]
-[[]]
-
-[1,[2,[3,[4,[5,6,7]]]],8,9]
-[1,[2,[3,[4,[5,6,0]]]],8,9]`;
-
-const signals = test.split('\n\n').map(pair => pair.split('\n').map(p => JSON.parse(p)));
+const signals = input.split('\n\n').map(pair => pair.split('\n').map(p => JSON.parse(p)));
 
 let ans1 = 0;
 
-const getArrayValue = (array) => {
-  const val = array.shift();
+const isValidSignal = (val1, val2, signal) => {
+  const isNumber1 = typeof val1 === 'number';
+  const isNumber2 = typeof val2 === 'number';
 
-  if (typeof val === 'object') {
-    if (!val.join()) return val.length.toString();
-    return val.join();
-  }
-
-  return val.toString();
-}
-
-const isValidSignal = (val1, val2) => {
-  let signal = 1;
-
-  while (val1.length && val2.length) {
-    const length1 = typeof val1[0] === 'object' && val1[0].length;
-    const length2 = typeof val2[0] === 'object' && val2[0].length;
-    if (length1 && length2 && length1 !== length2) {
-      const item2 = val2[0];
-
-      val1[0].some((item, index) => {
-        if (!item2[index]) return true;
-
-        item2[index] = typeof item2[index] === 'number' ? [item2[index]] : item2[index];
-        if (typeof item === 'object' && item.length > item2[index].length) {
-          item2[index].push(...Array(item.length - item2[index].length).fill(0));
-        }
-      });
-
-      val2[0] = item2;
+  if (isNumber1 && isNumber2) {
+    if (val1 < val2) {
+      signal.value = true;
+      return;
     }
-    const a1 = getArrayValue(val1);
-    const a2 = getArrayValue(val2);
+    else if (val1 > val2) {
+      signal.value = false;
+      return;
+    }
+  } else if (isNumber1 || isNumber2) {
+    if (isNumber1) isValidSignal([val1], val2, signal);
+    else isValidSignal(val1, [val2], signal);
+  } else {
+    let i = 0;
+    const len1 = val1.length;
+    const len2 = val2.length;
 
-    if (a1 > a2) {
-      signal = false;
-      break;
-    } else if (a1 < a2) {
-      signal = true;
-      break;
+    while (true) {
+      if (i > len1 - 1 && i < len2) {
+        signal.value = true;
+        return;
+      } else if (i < len1 && i > len2 - 1) {
+        signal.value = false;
+        return;
+      } else if (i > len1 - 1 && i > len2 - 1) {
+        return;
+      }
+
+      isValidSignal(val1[i], val2[i], signal);
+
+      if (typeof signal.value !== "undefined") {
+        return;
+      }
+
+      i++;
     }
   }
-
-  if (signal === 1 && val1.length) return false;
-
-  return signal;
 }
 
 signals.forEach((pair, index) => {
-  if (isValidSignal(pair[0], pair[1])) {
+  let signal = {};
+  isValidSignal(pair[0], pair[1], signal);
+  if (signal.value) {
     ans1 += index + 1;
   }
 });
